db0f3561646e16b6bf1b65d642b24deb418116fc
anders83k <anders83k@icloud.com>
2026-02-23 13:57:09 +0100

    Add files via upload
    java filer initial

---
+++ DrinkstogoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.jensen.drinkstogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkstogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
