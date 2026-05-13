2f75540c9ad99cc6febe64fb0aa693db596a7c64
anders83k <anders83k@icloud.com>
2026-05-13 15:23:46 +0200

    fixad version med generisk yaml

--- src/main/java/edu/jensen/drinkstogo/controller/SidesController.java
+++ src/main/java/edu/jensen/drinkstogo/controller/SidesController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import edu.jensen.drinkstogo.entity.Sides;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/sides")
 public class SidesController {
